35a2964528bc071e93c88ba291570d76855f915b
skatlqja3 <skatlqja3@naver.com>
2021-12-25 22:46:45 +0900

    Add Test 9498.py

---
+++ 9498.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+if 90 <= n <= 100:
+    print("A")
+elif 80 <= n <= 89:
+    print("B")
+elif 70 <= n <= 79:
+    print("C")
+elif 60 <= n <= 69:
+    print("D")
+else:
+    print("F")
