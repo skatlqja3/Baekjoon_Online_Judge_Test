bc00ef18ceaa33e062447881790a232f36eb07d7
skatlqja3 <skatlqja3@naver.com>
2022-01-20 23:57:14 +0900

    Add Test 1920.py

--- 1920.py
+++ 1920.py
@@ -1,8 +1,21 @@
-import sys
-n = int(sys.stdin.readline())
-l=[]
-for i in range(n):
-    l.append(int(sys.stdin.readline()))
-l.sort()
-for i in range(n):
-    print(l[i])
+from sys import stdin, stdout
+n = stdin.readline()
+a = sorted(map(int,stdin.readline().split()))
+m = stdin.readline()
+b = sorted(map(int,stdin.readline().split()))
+
+def binary(l, a, start, end):
+    if start > end:
+        return 0
+    m = (start+end)//2
+    if l == a[m]:
+        return 1
+    elif l < a[m]:
+        return binary(l, a, start, m-1)
+    else:
+        return binary(l, a, m+1, end)
+    
+for l in b:
+    start = 0
+    end = len(a)-1
+    print(binary(l,a,start,end))
