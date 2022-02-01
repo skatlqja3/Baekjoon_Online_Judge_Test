45aa27f281cd7cb9d217ef1cc13e1fd7948391bf
skatlqja3 <skatlqja3@naver.com>
2022-02-01 22:38:45 +0900

    Add Test 11650.py

---
+++ 11650.py
@@ -0,0 +1,8 @@
+import sys
+n=int(sys.stdin.readline())
+l=[]
+for _ in range(n):
+    l.append(list(map(int,sys.stdin.readline().split())))
+l=sorted(l,key=lambda x: (x[0],x[1]))
+for i in range(n):
+    print(f"{l[i][0]} {l[i][1]}")
