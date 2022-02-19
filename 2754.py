bc448c3d132a92a860a8bf933a6249f1a97e7b5e
skatlqja3 <skatlqja3@naver.com>
2022-02-19 23:29:02 +0900

    Add Test 2754.py

---
+++ 2754.py
@@ -0,0 +1,31 @@
+n=input()
+if 'A+' == n:
+    print('4.3')
+elif 'A0' == n:
+    print('4.0')
+elif 'A-' == n:
+    print('3.7')
+#
+elif 'B+' == n:
+    print('3.3')
+elif 'B0' == n:
+    print('3.0')
+elif 'B-' == n:
+    print('2.7')
+#
+elif 'C+' == n:
+    print('2.3')
+elif 'C0' == n:
+    print('2.0')
+elif 'C-' == n:
+    print('1.7')
+#
+elif 'D+' == n:
+    print('1.3')
+elif 'D0' == n:
+    print('1.0')
+elif 'D-' == n:
+    print('0.7')
+#
+else:
+    print('0.0')
