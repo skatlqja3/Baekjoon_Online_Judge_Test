329e78120281a64b805cf5569845be6aee093072
skatlqja3 <skatlqja3@naver.com>
2022-02-24 23:39:21 +0900

    Add Test 10886.py

---
+++ 10886.py
@@ -0,0 +1,10 @@
+n = int(input())
+count = 0
+for i in range(n):
+    m = int(input())
+    if m == 1:
+        count+=1
+if count > n//2:
+    print('Junhee is cute!')
+else:
+    print('Junhee is not cute!')
